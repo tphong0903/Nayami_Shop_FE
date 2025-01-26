649f6889a4b8a4b8a07001c08426defbfef405da
QuangDang <QuangDang@DESKTOP-588IDI4>
2025-01-26 20:53:51 +0700

    co ban product

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,13 +2,21 @@ import Header from '~/components/header/Header';
 import Footer from '~/components/footer/Footer';
 import BreadCrumbSection from '~/components/product/BreadCrumbSection';
 import SidebarSection from '~/components/product/SidebarSection';
+import ReletedProductSection from '~/components/product/ReletedProductSection';
+import StickyCartSection from '~/components/product/StickyCartSection';
+import QuickViewSection from '~/components/product/QuickViewSection';
+import DealBoxSection from '~/components/product/DealBoxSection';
 export default function ProductPage() {
   return (
     <>
       <Header />
       <BreadCrumbSection />
       <SidebarSection />
+      <ReletedProductSection />
       <Footer />
+      <QuickViewSection />
+      <DealBoxSection />
+      <StickyCartSection />
     </>
   );
 }
