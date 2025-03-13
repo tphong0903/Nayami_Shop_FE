ad029d8084f382702f37174f93ba7687cc42bc4d
TPhong <phongga088@gmail.com>
2025-03-13 11:54:31 +0700

    add list product UIUI

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
-    alias: [{
-      find: '~', replacement: '/src'
-    }]
-  }
-})
+    alias: {
+      '~': '/src',
+    },
+  },
+});
