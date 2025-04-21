fc98cd5f7ed1e83f00f20a70e80caf51257a08bc
TPhong <phongga088@gmail.com>
2025-04-21 13:08:20 +0700

    fix ui

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -9,34 +9,30 @@ import axios from 'axios';
 
 export default function UserDashboard() {
   const [informUser, setInformUser] = useState(false)
+  
   useEffect(() => {
     const fetchUserData = async () => {
       const token = localStorage.getItem('access_token');
       const email = getEmailFromToken(token);
-      console.log(email);
       if (!email) return;
       try {
-        console.log('Call API to get user information by email');
         const response = await axios.get(`/api/users/email/${email}`);
         const user = response.data;
-        console.log(user);
         localStorage.setItem('user_information', JSON.stringify(user));
         setInformUser(true);
-        console.log('Call API user successfully');
       } catch (error) {
         console.error('Error fetching user:', error);
       }
     };
-    console.log('Fetch user');
-    if(localStorage.getItem('user_information') == null) {
+    if (localStorage.getItem('user_information') == null) {
       fetchUserData();
     }
-  },[]);
+  }, []);
   return (
     <>
       <Header />
       <BreadCrumb title='Dashboard' page='Dashboard' />
-      <DashboardLayout/>
+      <DashboardLayout />
       <Footer />
     </>
   );
