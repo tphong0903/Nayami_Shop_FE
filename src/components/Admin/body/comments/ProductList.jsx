d1bc9b9008886197ec9ed4a88e5d7463be648911
BaoStar0990 <slamandertranbao2712@gmail.com>
2025-04-28 15:39:25 +0700

    fixed ui comment

--- src/components/Admin/body/comments/ProductList.jsx
+++ src/components/Admin/body/comments/ProductList.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import 'datatables.net-bs5'
 import '/src/assets/Admin/css/customPagination.css';
-import { Box, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material'
+import $ from 'jquery'
+import SwapVertIcon from '@mui/icons-material/SwapVert';
+import { Tooltip } from '@mui/material';
+import { Rating } from '@mui/material'
 
 export default function ProductList() {
 
     const [products, setProducts] = useState([])
+    const tableRef = useRef(null)
+    const navigate = useNavigate()
 
     const fetchData = async () => {
         await axios
@@ -19,6 +24,7 @@ export default function ProductList() {
             })
             .then((response) => {
                 setProducts(response.data.data)
+                console.log(response.data.data)
             })
             .catch(() => {
                 Swal.fire('Lỗi!', 'Không thể tải danh sách sản phẩm.', 'error')
@@ -30,9 +36,19 @@ export default function ProductList() {
         fetchData()
     }, [])
 
+    useEffect(() => {
+        if (products.length > 0) {
+            $(tableRef.current).DataTable()
+        }
+    }, [products])
+
     // useEffect(() => {
     //     console.log(products)
     // }, [products])
+
+    const handleRowClick = (id) => {
+        navigate(`/admin/comments/${id}`)
+    }
 
     return (
         <>
@@ -45,61 +61,68 @@ export default function ProductList() {
                                     <div className='title-header option-title'>
                                         <h5>Danh sách sản phẩm</h5>
                                     </div>
-                                    <Box className="p-4 bg-gray-100 min-h-screen">
-                                        <Grid container spacing={3}>
-                                            {
-                                                products.length > 0
-                                                    ? products.map((card, index) => {
-                                                        return (<>
-                                                            <Grid item xs={12} sm={6} md={3} key={index}>
-                                                                <Link to={`/admin/comments/${card.id}`} underline="none">
-                                                                    <Card
-                                                                        sx={{
-                                                                            boxShadow: 3,
-                                                                            '&:hover': { boxShadow: 6 },
-                                                                            transition: 'box-shadow 0.3s',
-                                                                            cursor: 'pointer',
-                                                                            overflow: 'hidden',
-                                                                            display: 'flex',
-                                                                            flexDirection: 'column',
-                                                                            height: '100%', // Let it fill the Grid item, but content will define minimum height
-                                                                        }}
-                                                                    >
-                                                                        <CardMedia
-                                                                            component="img"
-                                                                            image={card.listImage[0]}
-                                                                            alt={card.name}
-                                                                            sx={{
-                                                                                height: 160,
-                                                                                objectFit: 'contain',
-                                                                                width: '100%',
-                                                                                marginTop: 2,
-                                                                            }}
-                                                                            className="zoom-image"
-                                                                        />
-                                                                        <CardContent sx={{ textAlign: 'center', flexGrow: 1 }}>
-                                                                            <Typography
-                                                                                variant="h5"
-                                                                                component="div"
-                                                                                className="gradient-text"
-                                                                                sx={{
-                                                                                    fontFamily: "'Playfair Display', serif",
-                                                                                    fontWeight: 700,
-                                                                                }}
-                                                                            >
-                                                                                {card.name}
-                                                                            </Typography>
-                                                                        </CardContent>
-                                                                    </Card>
-                                                                </Link>
-                                                            </Grid>
-                                                        </>)
-                                                    })
-                                                    : <>Loading</>
-                                            }
-                                        </Grid>
-                                    </Box>
-
+                                    <div className='table-responsive category-table'>
+                                        <table
+                                            ref={tableRef}
+                                            className="table all-package theme-table table-product dataTable no-footer" id="table_id"
+                                        >
+                                            <thead>
+                                                <tr>
+                                                    <th>Hình ảnh</th>
+                                                    <th>Tên sản phẩm<SwapVertIcon /></th>
+                                                    <th>Danh mục<SwapVertIcon /></th>
+                                                    <th>Số lượng đánh giá<SwapVertIcon /></th>
+                                                    <th>Đánh giá<SwapVertIcon /></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {products.length > 0 ? (
+                                                    products.map((product) => (
+                                                        <tr
+                                                            key={product.id}
+                                                            onClick={() => handleRowClick(product.id)}
+                                                            style={{ cursor: 'pointer' }}>
+                                                            <td>
+                                                                <div className="table-image">
+                                                                    <img
+                                                                        src={product.listImage && product.listImage.length > 0 ? product.listImage[0] : null}
+                                                                        className="img-fluid"
+                                                                        alt={product.name}
+                                                                    />
+                                                                </div>
+                                                            </td>
+                                                            <Tooltip title={product.name} arrow>
+                                                                <td style={{
+                                                                    padding: '0px',
+                                                                    margin: '15px',
+                                                                    maxWidth: '300px',
+                                                                    display: '-webkit-box',
+                                                                    WebkitLineClamp: 2,
+                                                                    WebkitBoxOrient: 'vertical',
+                                                                    overflow: 'hidden',
+                                                                    textOverflow: 'ellipsis',
+                                                                    whiteSpace: 'normal',
+                                                                    wordBreak: 'break-word',
+                                                                    cursor: 'pointer'
+                                                                }}>
+                                                                    {product.name}
+                                                                </td>
+                                                            </Tooltip>
+                                                            <td>{product.categoryDTO.categoryName}</td>
+                                                            <td>{product.quantity}</td>
+                                                            <td><Rating defaultValue={product?.ratingAvg ?? 0} precision={1} size='medium' readOnly /></td>
+                                                        </tr>
+                                                    ))
+                                                ) : (
+                                                    <tr>
+                                                        <td colSpan={7} className='text-center'>
+                                                            Không có sản phẩm nào
+                                                        </td>
+                                                    </tr>
+                                                )}
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
