81f8aa0f31e7654002e70465c3f2e2e5fc947430
Thanh Phong <115602336+tphong0903@users.noreply.github.com>
2025-05-03 19:42:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://nayami-shop-fe.vercel.app/',
   plugins: [react()],
   server: {
     proxy: {
