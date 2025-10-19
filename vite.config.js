4fd68424b3ce7e1f410a5ccdb6bda03326910b34
TPhong <phongga088@gmail.com>
2025-10-19 21:26:18 +0700

    https

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://nayami-shop-fe.vercel.app/',
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
     proxy: {
       '/api': {
         // target: 'https://nayamishop.id.vn',
-        target: 'http://localhost:8081',
+        target: 'https://localhost:8081',
         changeOrigin: true,
         secure: false,
       },
